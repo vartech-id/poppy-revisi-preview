93e05da02e5cb30e0f5e7c99cbdc8d1081b35499
PvtMilo <flip360.id.solo.@gmail.com>
2025-12-01 13:44:03 +0700

    update router index untuk check in

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Form from '../views/Form.vue'
 import Last from '../views/Last.vue'
-import checkIn from '../views/check-in.vue'
-import CheckIn from '../views/check-in.vue'
+import check_in from '../views/check_in.vue';
+import check_in_success from '../views/check_in_success.vue';
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/form', name: 'form', component: Form },
   { path: '/last', name: 'last', component: Last },
-  { path: '/check-in', name: 'check', component: CheckIn}  
+  { path: '/check-in', name : 'check_in', component : check_in},
+  { path: '/check-in-success', name: 'check-in-success', component: check_in_success}  
 ]
 
 const router = createRouter({
