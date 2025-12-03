da46cc041af4db5beeda92c8c1ca0c9f4c22ad58
PvtMilo <flip360.id.solo.@gmail.com>
2025-12-03 13:33:24 +0700

    update tampilan landscape check in final

--- src/views/CheckIn.vue
+++ src/views/CheckIn.vue
@@ -6,14 +6,17 @@
   <div id="content-wrapper">
     <div class="content">
       <img class="logo-poppy" src="./assets/mainLogo.png" alt="logo_poppy" />
-      <span>Scan The Qr to the QR code </span>
+      <span class="scan-label"> SCAN QR DISINI </span>
+      <div class="info-wrapper">
+        <p>Check whatsapp anda kembali, dan temukan QR yang telah kami kirimkan <br> saat anda selesai melakukan kegiatan registrasi</p>
+      </div>
     </div>
   </div>
 </template>
 <style scoped>
 #content-wrapper {
   height: 100vh;
-  background-image: url("./assets/bg_landscape.jpg");
+  background-image: url("./assets/bg_landscape.png");
   background-position: center;
   background-repeat: no-repeat;
   background-size: contain;
@@ -26,17 +29,34 @@
   height: 100vh;
   align-items: center;
   justify-content: center;
-  gap: 5em;
-  padding-bottom: 3em;
+  gap: 2.2em;
+  padding-bottom: 4.5em;
 }
 
 .logo-poppy {
   width: 35%;
 }
 
-span {
-  font-size: 1.5em;
+.scan-label{
+  background-color: #0e4425;;
+  width: 20%;
+  font-size: 2.2em;
+  width: 40%;
+  height: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Avenir", sans-serif;
+  color: #ffffff;
+  border: 1px solid;
+  border-radius: 9999px;
+}
+
+.info-wrapper{
+  text-align: center;
   font-family: "Avenir", sans-serif;
   color: #0e4425;
+  font-size: 1.5em;
 }
+
 </style>
--- src/views/CheckIn_success.vue
+++ src/views/CheckIn_success.vue
@@ -4,12 +4,26 @@
   <div id="content-wrapper">
     <div class="content">
       <img class="logo-poppy" src="./assets/mainLogo.png" alt="logo_poppy" />
+      <span class="label-berhasil-scan"> Qr anda telah berhasil di scan, silahkan mengikuti dan memasuki area event! </span>
       <div class="detail-wrapper">
-        <span>Nama : Samuel Ilham </span>
-        <span>No Whatsapp : 085792190222</span>
-        <span>Check-in: 30-Nov-2025 16:43 WIB </span>
-        <span>Jumlah Tamu : 2 </span>
-        <span>Status : Qr Sudah Pernah digunakan sebelumnya </span>
+        <div class="label-input-tamu">
+          <span class="label-nama">Nama :</span>
+          <span class="label-wa">No. Whatsapp :</span>
+          <span class="label-jumlah-tamu">Jumlah Tamu :</span>
+          <span class="label-waktu-check-in">Waktu Check-In :</span>          
+        </div>
+        <div class="data-tamu">
+          <span class="input-nama">Data paling panjang sudah pas tidak kemana</span>
+          <span class="input-wa">085792190211</span>
+          <span class="input-jumlah">2 Orang</span>
+          <span class="input-checkin"> 16.30 WIB </span>
+        </div>
+        <div class="status-qr-wrapper"> 
+          <span class="status-title">STATUS</span>
+          <span class="status-qr"> QR SUDAH PERNAH DIGUNAKAN SEBELUMNYA
+          </span>
+        </div>
+        
       </div>
     </div>
   </div>
@@ -17,7 +31,7 @@
 <style scoped>
 #content-wrapper {
   height: 100vh;
-  background-image: url("./assets/bg_landscape.jpg");
+  background-image: url("./assets/bg_success.png");
   background-position: center;
   background-repeat: no-repeat;
   background-size: contain;
@@ -30,24 +44,87 @@
   height: 100vh;
   align-items: center;
   justify-content: center;
-  gap: 0em;
-  padding-bottom: 3em;
+  row-gap: 2em;
+  padding-bottom: 4em;
 }
 
 .logo-poppy {
-  width: 35%;
+  width: 28%;
+}
+
+.label-berhasil-scan{
+  font-size: 1.4em;
+  font-family: "Avenir", sans-serif;
+  color: #024123;
 }
 
 .detail-wrapper{
     display: flex;
-    flex-direction: column;
-    text-align: center;
-    padding-top: 2em;
+    flex-direction: row;
+    justify-content: center;
+    column-gap: 1em; 
+    font-family: "Avenir", sans-serif;
+    color: #024123;
+    /* background-color: blanchedalmond; */
+    width: 100%;
 }
 
-span {
-  font-size: 2.3em;
-  font-family: "Avenir", sans-serif;
-  color: #0e4425;
+.label-input-tamu,
+.data-tamu{
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.8em;
 }
+
+.label-input-tamu{
+  text-align: right;
+}
+
+.data-tamu{
+  text-align: center;
+  border: 1px solid white;
+  width: 400px;
+}
+
+.status-qr-wrapper{
+  display: flex;
+  flex-direction: column;
+  padding-left: 2em;
+  /* background-color: rgba(0, 253, 253, 0.274); */
+  width: 20%;
+}
+
+.status-title{
+  font-size: 1em;
+}
+
+.status-qr{
+  padding-top: 1.7em;
+  font-size: 1.2em;
+  
+}
+
+/* label detail nama no wa jumlah tamu wajtu check in */
+.label-input-tamu , .data-tamu {
+  font-size: 1.2em;
+}
+
+
+/* input data nama,wa,jumlah tamu,waktu check-in */
+.input-nama, .input-wa, .input-jumlah, .input-checkin{
+  background-color: rgb(255, 255, 255);
+  border : 2px solid #024123;
+  border-radius: 9999px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.label-input-tamu > * , .input-nama, .input-wa, .input-jumlah, .input-checkin{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 2.2em;
+}
+
 </style>
